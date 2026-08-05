dc171e1ca0a522e0152cfff25c264d4988a05cc8
TanishRaghwani2130 <tanishraghwani25@gmail.com>
2026-08-06 03:15:50 +0530

    Delete backend directory

--- backend/app/graph/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Investigation graph package exports."""
-
-from app.graph.builder import investigation_graph
-from app.graph.workflow import run_investigation
-
-__all__ = [
-    "investigation_graph",
-    "run_investigation",
-]
